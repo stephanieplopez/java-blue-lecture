5051e97a98270b19cab5f0117eb30f0812408829
Stephanie <stephanieplopez@gmail.com>
2019-03-28 09:26:46 -0400

    pulling lecture

---
+++ m4-06_Introduction_to_Vue-lecture/vue-product-reviews/src/components/ProductReview.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main">
+        <h2>Product Review {{ productName }}</h2>
+        <p class="description">{{ description }}</p>
+    </div>
+</template>
+
+<style scoped>
+div.main {
+    margin: 1rem 0;
+}
+</style>
+
+<script>
+export default {
+    // What needs to be exported (COMMA), these are key value pairs
+    name: 'product-review',
+    // Putting in variables, must return a JavaScript object
+    data() {
+        return {
+            // One way databinding
+            productName: "Cigar Parties for Dummies",
+            description: "Host and plan the perfect cigar party for all your squirrelly friends."
+        }
+    }
+}
+</script>
+
